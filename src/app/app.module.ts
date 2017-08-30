2e00a3707838b9ca501fd3c675ab951c50cab954
Glenn <Glenn@health-cast.com>
2017-08-30 12:59:58 +0200

    minimal example to recreate bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
@@ -11,6 +12,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
+import { NgxChartsModule } from "@swimlane/ngx-charts/release/ngx-charts.module";
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,6 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+		BrowserAnimationsModule,
+		NgxChartsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
